983aea6f075e2032f76027f9cfd6ee338604ca80
Sushant <souravsushant007@gmail.com>
2024-04-03 01:44:07 +0530

    Updated Files

--- Attendance_App/face_detection.py
+++ Attendance_App/face_detection.py
@@ -1,10 +1,7 @@
 import cv2
 
-# Load the pre-trained Haar cascade for face detection
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Load an image or capture from webcam
-# For example, to capture from webcam:
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -12,23 +9,17 @@ while True:
     if not ret:
         break
 
-    # Convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Detect faces in the grayscale image
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    # Draw bounding boxes around the detected faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    # Display the result
     cv2.imshow('Face Detection', frame)
 
-    # Break the loop when 'q' is pressed
     if cv2.waitKey(1)== 13:
         break
 
-# Release the video capture object and close all windows
 cap.release()
 cv2.destroyAllWindows()
